cb1dde8b0cc32922cc582ef7ff2fc4cf2cd9c14a
Ovindu Manawaduge <odmanawaduge@gmail.com>
2020-08-03 10:37:17 +1000

    update `fshow`

--- config/aliases.sh
+++ config/aliases.sh
@@ -5,10 +5,10 @@ alias cls="clear"
 fshow() {
   git log --graph --color=always \
       --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
-  fzf --ansi --preview "echo {} | grep -o '[a-f0-9]\{7\}' | head -1 | xargs -I % sh -c 'git show --color=always %'" \
+  fzf --ansi --preview "echo {} | grep -o '[a-f0-9]\{7\}' | head -1 | xargs -I % sh -c 'git show % | diff-so-fancy'" \
              --bind "enter:execute:
                 (grep -o '[a-f0-9]\{7\}' | head -1 |
-                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                xargs -I % sh -c 'git show % | diff-so-fancy | less -R') << 'FZF-EOF'
                 {}
 FZF-EOF"
 }

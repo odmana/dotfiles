4ef44c519ec6bd03a291451e1a7dee315d33ceb4
Ovindu Manawaduge <odmanawaduge@gmail.com>
2020-08-17 16:50:49 +1000

    Git `openpr` linux support

--- config/aliases.sh
+++ config/aliases.sh
@@ -114,7 +114,13 @@ openpr() {
     github_url=`git remote -v | awk '/fetch/{print $2}' | sed -Ee 's#(git@|git://)#https://#' -e 's@com:@com/@' -e 's%\.git$%%' | awk '/github/'`;
     branch_name=`git symbolic-ref HEAD | cut -d"/" -f 3,4`;
     pr_url=$github_url"/compare/master..."$branch_name
-    open $pr_url;
+
+    if command -v open &> /dev/null
+    then
+      open $pr_url;
+    else
+      xdg-open $pr_url;
+    fi
   else
     echo 'failed to push commits and open a pull request.';
   fi

6fa7e43250a8ab4651dfda06ca4e07b78191c51e
Ovindu Manawaduge <odmanawaduge@gmail.com>
2021-02-14 23:14:56 +1000

    Update `once_mac` script

--- once_mac.sh
+++ once_mac.sh
@@ -1,32 +1,28 @@
-# Install Homebrew /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+## Install Homebrew before running ##
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# Install tools
+brew install cask
 brew install diff-so-fancy
-
-brew cask install alacritty
-
 brew install bat
-
 brew install fzf
-
 brew install tmux
 curl -L https://git.io/tmux-up -o /usr/local/bin/tmux-up
 
+# Install "Tmux Plugin Manager" Hit `prefix + I` to setup plugins specified in `tmux.conf`
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Install terminal
+brew install --cask alacritty
+
+# Install shell (zsh) and set as default
 brew install zsh
-
-# Install "Oh my ZSH"
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sudo chsh -s /bin/zsh
-
-# Install "Powerlevel9k Zsh Theme"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/romkatv/powerlevel10k.git  ~/.oh-my-zsh/custom/themes/powerlevel10k
-
-# Install "Zsh Autosuggestions", "Zsh Completions", "Zsh Syntax Highlighting"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-# Install "Tmux Plugin Manager"
-# Hit `prefix + I` to setup plugins specified in `tmux.conf`
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
+# Symlink dotfiles
 ./create-links.sh

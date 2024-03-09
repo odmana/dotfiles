c4c27ec688b0cfd71aa39bb356e77f7a259b24c9
Ovindu Manawaduge <odmanawaduge@gmail.com>
2024-03-09 15:16:00 +1000

    Add unix init for terminal based containers

---
+++ once_unix.sh
@@ -0,0 +1,24 @@
+## run with sudo ##
+
+# Install tools
+apt install curl
+apt install git
+apt install vim
+apt install bat
+apt install fzf
+apt install tmux
+
+# Install tmux plugin manager (Hit `prefix + I` to setup plugins specified in `tmux.conf`)
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Install shell (zsh) and set as default
+apt install zsh
+chsh -s /bin/zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/romkatv/powerlevel10k.git  ~/.oh-my-zsh/custom/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+# Symlink dotfiles
+./create-links.sh
